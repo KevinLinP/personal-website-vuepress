a8c4623f889ae6d20ee6570e67b6d09867047330
Kevin Lin <kevin.lin.p@gmail.com>
2018-08-07 06:58:11 -0700

    homepage layout

---
+++ .vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  // link()
+  head: [
+    ['link', {href: "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css", rel: "stylesheet", integrity: "sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO", crossorigin: "anonymous"}],
+    ['link', {rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.2/gh-fork-ribbon.min.css", integrity: "sha256-8WPvbdgP5IJUoTFanScEKvXvkBqdSHV35GXwi/jUzw8=", crossorigin: "anonymous"}],
+    ['script', {src: "https://code.jquery.com/jquery-3.3.1.slim.min.js", integrity: "sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=", crossorigin: "anonymous", defer: 'true'}],
+    ['script', {src: "/jquery.parallax.min.js", defer: 'true'}]
+  ],
+  markdown: {
+    anchor: {
+      permalink: false
+    }
+  }
+}
---
+++ .vuepress/theme/Layout.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div
+    .parallax-window(data-parallax data-src="https://ddw2ppmkywo9u.cloudfront.net/sunset_cropped.jpg" data-bleed="120" data-speed="0.63")
+      | hi
+    .text-center
+      img(src='https://ddw2ppmkywo9u.cloudfront.net/profile_picture.jpg' class='circular-image white-border' height='150px' width='150px')
+    .container
+      Content
+</template>
+
+<style>
+  .parallax-window {
+    min-height: 50vh;
+    background: transparent;
+  }
+  .circular-image {
+    border-radius: 50%;
+  }
+  .white-border {
+    border: 3px solid white
+  }
+</style>
